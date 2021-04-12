b5ff3c50f40d173a291c4f007f3fc89deef0d7d1
Damien <249016681@qq.com>
2021-04-12 17:44:24 +0800

    Update provicer.tf

--- uat-kafka/provicer.tf
+++ uat-kafka/provicer.tf
@@ -1,1 +1,24 @@
 
+terraform {
+  required_providers {
+    tencentcloud = {
+      source = "tencentcloudstack/tencentcloud"
+      version = "1.53.8"
+    }
+
+    ansible = {
+      source = "nbering/ansible"
+      version = "1.0.4"
+    }
+
+  }
+}
+
+
+
+
+provider "tencentcloud" {
+    secret_id  = "AKIDVXN9mttZDKtHTUnub7RccR4exAB0iIBs"
+    secret_key = "maCc2d5LkU7NZe3Sflx0wFet0DICaweQ"
+    region     = "ap-guangzhou"
+}
